8a90699763dcea3defe0a153dd988aa3d97331b6
mosuswalks <mosuswalks@gmail.com>
2019-02-04 18:15:03 -0500

    Complete backend change using graphql-cli"

--- backend/build/graphql/schemas/User.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var graphql_1 = require("graphql");
-var typeDefs = graphql_1.buildSchema("\n    type User {\n        _id: Int!\n        firstName: String\n        lastName: String\n        email: String\n        password: String\n    }\n");
