d05eb58145197e2a0ae164925ddf55096b2dc2d6
Carol Hung <caroljhung@gmail.com>
2019-04-08 06:17:31 -0400

    modify front end scraper ui

--- frontend/src/pages/EventTaskPage.tsx
+++ frontend/src/pages/EventTaskPage.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import EventList from "../components/EventComponents/EventList";
+import BlogtoEventList from './../components/EventComponents/BlogtoEventList';
 
 export default class SchedulePage extends Component {
   render() {
     return (
       <div>
         <EventList userid = ""/>
+        <BlogtoEventList />
       </div>
     );
   }
